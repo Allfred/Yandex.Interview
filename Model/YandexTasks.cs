eed307ff671cee33b032047c77c9a7fe63d93174
Allfred <alfredviktorov@gmail.com>
2019-10-02 14:23:30 +0300

    TaskA

--- Model/YandexTasks.cs
+++ Model/YandexTasks.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 namespace Yandex.Interview.Model
 {
     public static class YandexTasks
-    {
+    {   
+        
+        
+        /// <summary>
+        /// Камни и украшения 
+        /// </summary>
+        public static void TaskA()
+        {
+            string line1 = Console.ReadLine();
+            string line2 = Console.ReadLine();
+
+            int count = 0;
+            foreach (char ch in line2)
+            {
+                if (line1.IndexOf(ch) >= 0)
+                {
+                    ++count;
+                }
+            }
+            Console.WriteLine(count);
+        }
     }
 }
